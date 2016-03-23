2081723a3fafb10165d1345528b095aab3df2c01
vsen <425268516@qq.com>
2016-03-23 15:51:48 +0800

    no message

--- apache2-php5.6.19/scripts/run.sh
+++ apache2-php5.6.19/scripts/run.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
-
 # execute any pre-init scripts, useful for images
 # based on this image
 for i in /scripts/pre-init.d/*sh
 do
-	if [ -e "${i}" ]; then
-		echo "[i] pre-init.d - processing $i"
-		. "${i}"
-	fi
+        if [ -e "${i}" ]; then
+             echo "[i] pre-init.d - processing $i"
+             ."${i}"
+        fi
 done
-
-# set apache as owner/group
+#set apache as owner/group
 if [ "$FIX_OWNERSHIP" != "" ]; then
-	chown -R apache:apache /app
+    chown -R apache:apache /app
 fi
 
 # display logs
-tail -F /var/log/apache2/*log &
+#tail -F /var/log/apache2/*log
 
 # execute any pre-exec scripts, useful for images
 # based on this image
 for i in /scripts/pre-exec.d/*sh
 do
-	if [ -e "${i}" ]; then
-		echo "[i] pre-exec.d - processing $i"
-		. "${i}"
-	fi
+        if [ -e "${i}" ]; then
+             echo "[i] pre-exec.d - processing $i"
+             ."${i}"
+        fi
 done
 
 echo "[i] Starting daemon..."
